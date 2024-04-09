cb0442fce465abc4c4048e0638f0ae674fc7a069
Tan Jun Yu <tjun-yu@u82z01s03.42kl.edu.my>
2024-04-09 15:27:52 +0800

    main.c: implemented simple window system with exit function

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
-#include "minilibx/mlx.h"
+#include <stdlib.h>
+#include "mlx.h"
 
-int main()
+void drawLine(void *mlx, void *win, int start_x, int start_y, int end_x, int end_y, int color);
+int	key_hook(int keycode);
+int quit();
+
+int main (void)
 {
-	void *mlx;
-	void *win;
+	void *mlx = mlx_init();
+	void *window = mlx_new_window(mlx, 500, 500, "test");
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 500, 500, "Hello, world!");
+	mlx_pixel_put(mlx, window, 500/2, 500/2, 0xFFFFFF);
+
+	mlx_key_hook(window, key_hook, &mlx);
+	mlx_hook(window, 17, 0, quit, mlx);
 	mlx_loop(mlx);
+
+	return (0);
+}
+
+void drawLine(void *mlx, void *win, int start_x, int start_y, int end_x, int end_y, int color)
+{
+	
+}
+
+int key_hook(int keycode)
+{
+	printf("keycode: %d\n", keycode);
+	if (keycode == 53)
+		exit(0);
 	return 0;
+}
+
+int quit()
+{
+	exit(EXIT_SUCCESS);
+	return (0);
 }
