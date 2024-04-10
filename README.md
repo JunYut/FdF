dcb6fa5b01cdbc2947d5ed74fe4f7de71ecd1b4f
Tan Jun Yu <tjun-yu@u82z01s03.42kl.edu.my>
2024-04-10 15:55:16 +0800

    Just use Bersenham's line algorithm

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 # 3 Cases
 ## 1. Gradient is integer
 ## 2. Gradient is fraction
-Mulitply coefficient of `x` and `y` until 
+Mulitply coefficient of `x` and `y` by `10` until `y` is greater than `10`, then simplify both coefficients.
+
+
+dx = x1 - x0
+dy = y1 - y0
+
+if 
--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "mlx.h"
 
+void drawLine(void *mlx, void *win, int x1, int y1, int x2, int y2, int color);
 void drawGrid(void *mlx, void *win, int x, int y, int color);
 int	key_hook(int keycode);
 int quit();
@@ -11,11 +12,9 @@ int main (void)
 	void *mlx = mlx_init();
 	void *window = mlx_new_window(mlx, 500, 500, "test");
 
-	printf("floating point: %.40lf\n", 4 / 3.0);
-
-	mlx_pixel_put(mlx, window, 500/2, 500/2, 0xFFFFFF);
-
 	drawGrid(mlx, window, 500, 500, 0xFFFFFF);
+
+	drawLine(mlx, window, 0, 0, 100, 200, 0xFF0000);
 
 	mlx_key_hook(window, key_hook, &mlx);
 	mlx_hook(window, 17, 0, quit, mlx);
@@ -24,11 +23,30 @@ int main (void)
 	return (0);
 }
 
+void drawLine(void *mlx, void * win, int x1, int y1, int x2, int y2, int color)
+{
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int steps = abs(dx) > abs(dy) ? abs(dx) : abs(dy);
+	float Xinc = dx / (float)steps;
+	float Yinc = dy / (float)steps;
+	float X = x1;
+	float Y = y1;
+
+	printf("dx: %d, dy: %d, steps: %d, Xinc: %f, Yinc: %f\n", dx, dy, steps, Xinc, Yinc);
+	for (int i = 0; i <= steps; i += 1)
+	{
+		mlx_pixel_put(mlx, win, X, Y, color);
+		X += Xinc;
+		Y += Yinc;
+	}
+}
+
 void drawGrid(void *mlx, void *win, int x, int y, int color)
 {
 	for (int i = 0; i <= x; i += 1)
 		for (int j = 0; j <= y; j += 1)
-			if (i % 25 == 0 || j % 25 == 0)
+			if (i % 10 == 0 || j % 10 == 0)
 				mlx_pixel_put(mlx, win, i, j, color);
 }
 
