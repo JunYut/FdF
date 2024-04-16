a3de2e6431be126fe15fe6cdec664c959b527b3f
Tan Jun Yu <tjun-yu@u82z01s03.42kl.edu.my>
2024-04-16 15:34:53 +0800

    src/parser.c: added looping mechanism for linked list

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 13:20:08 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/04/16 15:02:14 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2024/04/16 15:24:49 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@ t_map	*parse_map(int fd)
 {
 	t_map		*map;
 	t_parser	parser;
+	t_list		*current;
 	int			i;
 
 	map = (t_map *)c_malloc(sizeof(t_map));
 	parser.list = ft_lstnew(0);
+	current = parser.list;
 	parser.str = get_next_line(fd);
 	while (parser.str)
 	{
 		parser.split = ft_split(parser.str, ' ');
 		parser.columns = count_words(parser.split);
-		parser.list->content = (int *)c_malloc(sizeof(int) * parser.columns);
+		current = (int *)c_malloc(sizeof(int) * parser.columns);
 
 		i = -1;
 		while (parser.split[++i])
-			((int *)parser.list->content)[i] = ft_atoi(parser.split[i]);
+			((int *)current)[i] = ft_atoi(parser.split[i]);
 		ft_lstadd_back(&parser.list, ft_lstnew(0));
+		current = current->next;
 
 		free_split(parser.split);
 		c_free(parser.str);
