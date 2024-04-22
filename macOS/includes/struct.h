c625fc0c30f083515b7c3123f5809d7150616aad
Tan Jun Yu <tjun-yu@u91z05s03.42kl.edu.my>
2024-04-22 14:01:33 +0800

    macOS/controls.c: now there are few keys that can do shit when I press those keys on the window

--- macOS/includes/struct.h
+++ macOS/includes/struct.h
@@ -9,6 +9,31 @@
 # define WIN_WIDTH 800
 # define WIN_HEIGHT 600
 
+enum colors
+{
+	BLACK = 0x000000,
+	WHITE = 0xFFFFFF,
+	RED = 0xFF0000,
+	GREEN = 0x00FF00,
+	BLUE = 0x0000FF,
+	YELLOW = 0xFFFF00,
+	CYAN = 0x00FFFF,
+	MAGENTA = 0xFF00FF
+};
+
+enum keys
+{
+	ESC = 53,
+	SPACE = 49,
+	LEFT = 123,
+	RIGHT = 124,
+	UP = 126,
+	DOWN = 125,
+	W = 13,
+	A = 0,
+	S = 1,
+	D = 2
+};
 typedef struct	s_image
 {
 	void *img;
@@ -18,6 +43,13 @@ typedef struct	s_image
 	int endian;
 	int	offset;
 }				t_image;
+
+typedef struct	s_mlx
+{
+	void *mlx;
+	void *win;
+	t_image img;
+}				t_mlx;
 
 typedef struct	s_point
 {
@@ -36,5 +68,9 @@ typedef struct	s_line
 void drawPixel(t_image *img, int x, int y, int color);
 void drawLine(void *mlx, void *win, int x1, int y1, int x2, int y2, int color);
 void drawGrid(void *mlx, void *win, int x, int y, int color);
-int	key_hook(int keycode);
-int quit();
+int	key_hook(int keycode, t_mlx *mlx);
+int quit(t_mlx *mlx);
+int up(void);
+int down(void);
+int left(void);
+int right(void);
--- macOS/main.c
+++ macOS/main.c
@@ -7,24 +7,22 @@
 int main (void)
 {
 
-	void *mlx;
-	void *win;
-	t_image img;
+	t_mlx mlx;
 
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "mlx 42");
-	img.img = mlx_new_image(mlx, WIN_WIDTH, WIN_HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-	img.offset = (WIN_HEIGHT * img.line_length + WIN_WIDTH * (img.bits_per_pixel / 8));
-	drawPixel(&img, 100, 100, 0xFFFFFF);
-	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
+	mlx.mlx = mlx_init();
+	mlx.win = mlx_new_window(mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "mlx 42");
+	mlx.img.img = mlx_new_image(mlx.mlx, WIN_WIDTH, WIN_HEIGHT);
+	mlx.img.addr = mlx_get_data_addr(mlx.img.img, &mlx.img.bits_per_pixel, &mlx.img.line_length, &mlx.img.endian);
+	mlx.img.offset = (WIN_HEIGHT * mlx.img.line_length + WIN_WIDTH * (mlx.img.bits_per_pixel / 8));
+	drawPixel(&mlx.img, 100, 100, 0xFFFFFF);
+	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img.img, 0, 0);
+	mlx_key_hook(mlx.win, key_hook, &mlx);
 
-	printf("bits_per_pixel: %d\n", img.bits_per_pixel);
-	printf("line_length: %d\n", img.line_length);
-	printf("endian: %d\n", img.endian);
-	printf("offset: %d\n", img.offset);
-
-	mlx_loop(mlx);
+	mlx_loop(mlx.mlx);
 
 	return (0);
 }
+
+
+
+
