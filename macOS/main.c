78200164e5d7060cfb62cc7844d4c8eb6e6b778e
Tan Jun Yu <tjun-yu@u91z05s03.42kl.edu.my>
2024-04-22 09:48:25 +0800

    macOS/Makefile: got it to compile minilibx_macos

--- macOS/main.c
+++ macOS/main.c
@@ -1,22 +1,19 @@
 # include <stdio.h>
+# include <OpenGL/gl.h>
 # include "c_libft.h"
+# include "mlx.h"
 
 int main (void)
 {
-	t_list *list = ft_lstnew("Hello World!");
-	t_list	*current = list;
 
-	ft_lstadd_back(&list, ft_lstnew("This is the second node"));
-	ft_lstadd_back(&list, ft_lstnew("This is the third node"));
-	ft_lstadd_back(&list, ft_lstnew("This is the fourth node"));
+	void *mlx;
+	void *win;
+	void *img;
 
-	while (current != NULL)
-	{
-		printf("[%s]\n", (char *)current->content);
-		current = current->next;
-	}
-
-	ft_lstclear(&list, NULL);
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 800, 600, "mlx 42");
+	img = mlx_new_image(mlx, 800, 600);
+	mlx_loop(mlx);
 
 	return (0);
 }
