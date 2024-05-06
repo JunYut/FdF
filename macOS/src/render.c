854280a0caa1800cc7e909d3762825bd75422968
Tan Jun Yu <tjun-yu@u91z05s03.42kl.edu.my>
2024-05-06 14:29:36 +0800

    macOS/src/render.c: added input guard for 'renderPixel()'

--- macOS/src/render.c
+++ macOS/src/render.c
@@ -35,6 +35,9 @@ void renderPixel(t_img *img, int x, int y, int color)
 	char *dst;
 
 	// debug(NULL);
-	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+	if (x >= 0 && x < WIN_WIDTH && y >= 0 && y < WIN_HEIGHT)
+	{
+		dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+		*(unsigned int*)dst = color;
+	}
 }
