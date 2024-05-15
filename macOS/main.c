f465ae9b395b9ff9340fcb7b80e11ded03ba610c
Tan Jun Yu <tjun-yu@u91z05s03.42kl.edu.my>
2024-05-15 11:11:12 +0800

    TODO: redesign rendering mechanism

--- macOS/main.c
+++ macOS/main.c
@@ -6,48 +6,49 @@
 /*   By: tjun-yu <tjun-yu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 09:12:52 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/05/15 09:32:31 by tjun-yu          ###   ########.fr       */
+/*   Updated: 2024/05/15 11:13:02 by tjun-yu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FdF.h"
 
+static void	load_map(t_frame *f, int argc, char *argv[]);
+static void	init_mlx(t_frame *f);
+
 int	main(int argc, char *argv[])
 {
-	t_frame frame;
-	int		fd;
+	t_frame	frame;
 
-	ft_printf("Parsing map...\n");
-	fd = valid_arg(argc, argv[1]);
-	frame.map = parse_map(fd);
-	valid_map(frame.map);
-
-	// Initialize the mlx and create a window
-	ft_printf("Initializing system...\n");
-	frame.mlx.mlx = mlx_init();
-	frame.mlx.win = mlx_new_window(frame.mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "mlx 42");
-	frame.wireframe = init_wireframe(frame.map);
-	frame.redraw_flag = 1;
-
-	// Create an image
-	frame.mlx.img.img = mlx_new_image(frame.mlx.mlx, WIN_WIDTH, WIN_HEIGHT);
-	// newimg->buffer = malloc(UNIQ_BPP*width*height);
-
-	// printf("Image addr: %p\n", (void *)frame.mlx.img.img);	// Debug
-	frame.mlx.img.addr = mlx_get_data_addr(frame.mlx.img.img, &frame.mlx.img.bits_per_pixel, &frame.mlx.img.line_length, &frame.mlx.img.endian);
-	// printf("Addr: %p\n", (void *)frame.mlx.img.addr);	// Debug
-	frame.mlx.img.offset = (WIN_HEIGHT * WIN_WIDTH * (frame.mlx.img.bits_per_pixel / 8));
-	// printf("Offset: %d\n", frame.mlx.img.offset);	// Debug
-
-	// Set the loop hook and the key hook
+	load_map(&frame, argc, argv);
+	init_mlx(&frame);
 	ft_printf("Rendering...\n");
 	mlx_loop_hook(frame.mlx.mlx, render_new_frame, &frame);
-	mlx_hook(frame.mlx.win, 17, 0, quit, &frame);
-	mlx_hook(frame.mlx.win, 2, 1L<<0, key_hook, &frame);
-	mlx_key_hook(frame.mlx.win, key_hook, &frame);
-
-	// Loop
 	mlx_loop(frame.mlx.mlx);
-
 	return (0);
+}
+
+static void	load_map(t_frame *f, int argc, char *argv[])
+{
+	ft_printf("Parsing map...\n");
+	f->map = parse_map(valid_arg(argc, argv[1]));
+	valid_map(f->map);
+	f->wireframe = init_wireframe(f->map);
+}
+
+static void	init_mlx(t_frame *f)
+{
+	t_img	*img;
+
+	ft_printf("Initializing MinilibX components...\n");
+	f->mlx.mlx = mlx_init();
+	f->mlx.win = mlx_new_window(f->mlx.mlx, WIN_WIDTH, WIN_HEIGHT, "FdF");
+	img = &f->mlx.img;
+	img->img = mlx_new_image(f->mlx.mlx, WIN_WIDTH, WIN_HEIGHT);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
+	img->offset = (WIN_HEIGHT * WIN_WIDTH * (img->bits_per_pixel / 8));
+	mlx_hook(f->mlx.win, 17, 0, quit, f);
+	mlx_hook(f->mlx.win, 2, 0, key_hook, f);
+	mlx_key_hook(f->mlx.win, key_hook, f);
+	f->redraw_flag = 1;
 }
